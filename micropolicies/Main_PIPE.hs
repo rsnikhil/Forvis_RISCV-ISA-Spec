6857613a3d78838094c49a1ec6e351aa6e329d11
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2018-12-07 14:55:06 -0500

    CP - programs to be accepted and rejected

--- micropolicies/Main_PIPE.hs
+++ micropolicies/Main_PIPE.hs
@@ -69,12 +69,47 @@ main =
       addr_byte_list = []
       ms = mkMachine_State  RV32  misa  initial_PC  addr_ranges  addr_byte_list
 
-      mem' = (f_mem ms) { f_dm = Data_Map.fromList [(0, encode_I RV32 (ADDI 2 3 42))] }
-      ms' = ms { f_mem = mem'}
+      --- Generate input program + tags
+      -- Tags in call/ret f1-2-3-4-5
+      -- Memory colors in movs
+      -- add heap_base
+{-
+Put heap_base r1 @ default
+Mov r1 r1 @ (fresh color)
+Store r1 r1 @ default
+-}
+{- ACCEPT:
+Load r1 r2 @ default
+-}
+{- Reject:
+Put heap_base r2 @ default
+Load r2 r3 @ default
+-}
+      heap_base = 100
+      base_code =
+        [ (0, encode_I RV32 (ADDI 1 0 heap_base))
+        , (1, encode_I RV32 (ADD  1 0 1))
+        , (2, encode_I RV32 (SW   1 1 0))
+        ]
+      accept_code =
+        [ (3, encode_I RV32 (LW 2 1 0)) ]
+      reject_code =
+        [ (3, encode_I RV32 (ADDI 2 0 heap_base))
+        , (4, encode_I RV32 (LW 3 2 0)) ]
+      mem_acc = (f_mem ms) { f_dm = Data_Map.fromList (base_code ++ accept_code) }
+      mem_rej = (f_mem ms) { f_dm = Data_Map.fromList (base_code ++ reject_code) }
+      ms_acc = ms { f_mem = mem_acc }
+      ms_rej = ms { f_mem = mem_rej }
   in do
-    (n, ps, ms) <- run_loop 100 Nothing init_pipe_state ms' 
+    -- Here: pass the tags to pipe
+    (n, ps, ms) <- run_loop 100 Nothing init_pipe_state ms_acc
     putStrLn $ show (n, ps)
-    mstate_print "foo" ms
+    mstate_print "acc" ms
+
+    (n, ps, ms) <- run_loop 100 Nothing init_pipe_state ms_rej
+    putStrLn $ show (n, ps)
+    mstate_print "rej" ms
+
 
 -----------
 
--- micropolicies/PIPE.hs
+++ micropolicies/PIPE.hs
@@ -17,16 +17,59 @@ import Memory
 
 import Data.Bits
 
-opcodeE x   = shiftL x 0
-rdE x       = shiftL x 7
-funct3E x   = shiftL x 12
-rs1E x      = shiftL x 15
-rs2E x      = shiftL x 20
-funct7E x   = shiftL x 25
-imm12E x    = shiftL x 20
+--opcodeE x   = shiftL x 0
+--rdE x       = shiftL x 7
+--funct3E x   = shiftL x 12
+--rs1E x      = shiftL x 15
+--rs2E x      = shiftL x 20
+--funct7E x   = shiftL x 25
+--imm12E x    = shiftL x 20
 
 encode_I :: RV -> Instr_I -> Instr_32b
-encode_I rv (ADDI rd rs1 imm12) = opcodeE opcode_OP_IMM .|. rdE rd .|. rs1E rs1 .|. imm12E imm12
+encode_I rv (ADDI rd rs1 imm12) = mkInstr_I_type imm12 rs1 0 rd opcode_OP_IMM 
+encode_I rv (LW rd rs1 imm12)   = mkInstr_I_type imm12 rs1 funct3_LW rd opcode_LOAD
+encode_I rv (SW rd rs1 imm12)   = mkInstr_I_type imm12 rs1 funct3_SW rd opcode_STORE
+encode_I rv (ADD rd rs1 rs2)    = mkInstr_R_type funct7_ADD rs2 rs1 funct3_ADD rd opcode_OP
+
+mkInstr_I_type :: InstrField -> InstrField -> InstrField -> InstrField -> InstrField -> Instr_32b
+mkInstr_I_type    imm12         rs1           funct3        rd            opcode =
+  let
+    legal  = (((   shiftR  imm12  12) == 0)
+              && ((shiftR  rs1     5) == 0)
+              && ((shiftR  funct3  3) == 0)
+              && ((shiftR  rd      5) == 0)
+              && ((shiftR  opcode  7) == 0))
+
+    instr  = ((    shiftL  imm12   20)
+              .|. (shiftL  rs1     15)
+              .|. (shiftL  funct3  12)
+              .|. (shiftL  rd       7)
+              .|. opcode)
+  in
+    -- assert  legal  instr
+    instr
+
+mkInstr_R_type :: InstrField -> InstrField -> InstrField -> InstrField -> InstrField -> InstrField -> Instr_32b
+mkInstr_R_type    funct7        rs2           rs1           funct3        rd            opcode =
+  let
+    legal  = (((   shiftR  funct7  7) == 0)
+              && ((shiftR  rs2     5) == 0)
+              && ((shiftR  rs1     5) == 0)
+              && ((shiftR  funct3  3) == 0)
+              && ((shiftR  rd      5) == 0)
+              && ((shiftR  opcode  7) == 0))
+
+    instr = ((   shiftL  funct7  25)
+            .|. (shiftL  rs2     20)
+            .|. (shiftL  rs1     15)
+            .|. (shiftL  funct3  12)
+            .|. (shiftL  rd       7)
+            .|. opcode)
+  in
+    --    assert  legal  instr
+    instr
+
+
 
 --------------------------------------------------------
 
@@ -79,6 +122,14 @@ init_pipe_state = PIPE_State {
   }
 
 ---------------------------------
+
+exec_pipe_simple :: PIPE_State -> Machine_State -> Machine_State -> Integer -> IO (PIPE_State, Bool)
+exec_pipe_simple p m m' u32 =
+  let rv  = mstate_rv_read  m
+      res = decode_I rv u32
+  in case res of
+       Just (ADDI _ _ _) -> return (p,False)
+       _ -> return (p,False)
 
 exec_pipe :: PIPE_State -> Machine_State -> Machine_State -> Integer -> IO (PIPE_State, Bool)
 exec_pipe p m m' u32 =
