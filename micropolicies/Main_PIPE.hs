a13bae1dead6476be0d03bce722a1f860597d1cb
Benjamin Pierce <bcpierce002017@gmail.com>
2018-12-28 10:25:38 -0500

    [micro] Printing

--- micropolicies/Main_PIPE.hs
+++ micropolicies/Main_PIPE.hs
@@ -105,8 +105,8 @@ main3 = do
 main_mangled =
   quickCheck $ prop_noninterference bug_mangled_store_color
 
---main = main_mangled  
-main = mainHeap
+main = main_mangled  
+--main = mainHeap
   
 {-
   -- let (ms_acc, ms_rej) = exampleMachines
