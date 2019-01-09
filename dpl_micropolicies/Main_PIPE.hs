5c517a4325d50aa541e0be0ae0faaaca75a6c69d
Benjamin Pierce <bcpierce002017@gmail.com>
2019-01-09 14:51:36 -0500

    [micro] further experiments with policy interpreter / Stack

--- dpl_micropolicies/Main_PIPE.hs
+++ dpl_micropolicies/Main_PIPE.hs
@@ -1,7 +1,7 @@
 -- Copyright (c) 2018 Rishiyur S. Nikhil
 -- See LICENSE for license details
 
-module Main where
+module Main_PIPE where
 
 -- ================================================================
 -- This is the 'main' function of the program, which just dispatches
